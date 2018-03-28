58fedb6ab6138ebdaba2c6374ae15143718b7637
Mahima <Mahima@Mahimas-MacBook-Pro.local>
2018-03-28 10:10:10 -0400

    step one

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# Mukta Manhas
 # March 28, 2018
 
-###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+    """Convert the temperture given in Celsius to Fahrenheit"""
+    F = (1.8) * C + 32
+    return round(F)
 
 ###### uncomment this when you are ready to work on it
 #def FtoC ():
 #
 
 temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print(CtoF (temperature))
