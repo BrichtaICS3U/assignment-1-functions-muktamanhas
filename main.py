96fa7ab5aebbc8383e62694b0480d2c84fd6bb68
Mahima <Mahima@Mahimas-MacBook-Pro.local>
2018-03-28 10:30:48 -0400

    step 2

--- main.py
+++ main.py
@@ -4,13 +4,20 @@
 # March 28, 2018
 
 def CtoF (C):
-    """Convert the temperture given in Celsius to Fahrenheit"""
+    """Convert the temperature given in Celsius to Fahrenheit"""
     F = (1.8) * C + 32
     return round(F)
 
-###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
+def FtoC (F):
+    """Convert the temperature given in Fahrenheit to Celsius"""
+    C = (0.55556) * (F-32)
+    return round(C)
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(CtoF (temperature))
+#Ctemperature = int(input('Enter your temperature in Celsius: '))
+#Ftemperature = int(input('Enter your temperature in Fahrenheit: '))
+
+choice = int(input('Enter 1 for Celsius to Fahrenheit. Enter 2 for Fahrenheit to Celsius'))
+if choice == 1:
+        print (CtoF (int(input('Enter your temperature in Celsius: '))))
+elif choice == 2:
+        print (FtoC (int(input('Enter your temperature in Fahrenheit: '))))
