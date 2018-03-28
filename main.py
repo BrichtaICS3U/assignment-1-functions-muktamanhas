8c106925d284c35afd6575908fa197c5cd50b302
Mahima <Mahima@Mahimas-MacBook-Pro.local>
2018-03-28 11:01:19 -0400

    middle of step 4

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ def FtoC (F):
     C = (0.55556) * (F-32)
     return round(C)
 
-#Ctemperature = int(input('Enter your temperature in Celsius: '))
-#Ftemperature = int(input('Enter your temperature in Fahrenheit: '))
 
 choice = int(input('Enter 1 for Celsius to Fahrenheit. Enter 2 for Fahrenheit to Celsius'))
 if choice == 1:
-        print (CtoF (int(input('Enter your temperature in Celsius: '))))
+        degrees_cel = (int(input('Enter your temperature in Celsius: ')))
+        while degrees_cel < -274:
+            print (CtoF (int(input("Invalid temperature, below Absolute Zero. Enter new temperature in Celsius greater than -274: "))))
+            break
+        else:
+            print (CtoF (degrees_cel))
+        #if degrees_cel < -274:
+ #           print ("Invalid temperature, below Absolute Zero")
 elif choice == 2:
         print (FtoC (int(input('Enter your temperature in Fahrenheit: '))))
+
+else:
+    print (int(input('Enter 1 for Celsius to Fahrenheit. Enter 2 for Fahrenheit to Celsius')))
