52ad1bbbbb1e4dbd2aef44066174be56969dbfaa
Mahima <Mahima@Mahimas-MacBook-Pro.local>
2018-03-29 09:12:17 -0400

    final copy

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 def CtoF (C):
     """Convert the temperature given in Celsius to Fahrenheit"""
     F = (1.8) * C + 32
-    return round(F)
+    return round(F) 
 
 def FtoC (F):
     """Convert the temperature given in Fahrenheit to Celsius"""
@@ -20,26 +20,33 @@ def main():
     if choice == 1:
         degrees_cel = int(input('Enter your temperature in Celsius: '))
         while degrees_cel < -274:
-                degrees_cel = (int(input("Invalid temperature, below Absolute Zero. Enter new temperature in Celsius greater than -274: ")))
+                degrees_cel = (int(input('Invalid temperature, below Absolute Zero. Enter new temperature in Celsius that is greater than -274: ')))
         print (CtoF (degrees_cel))
             #if degrees_cel < -274:
-     #           print ("Invalid temperature, below Absolute Zero")
+               #print ("Invalid temperature, below Absolute Zero")
 
     elif choice == 2:
         degrees_fah = int(input('Enter your temperature in Fahrenheit: '))
         while degrees_fah < -460:
-            degrees_fah = (int(input("Invalid temperature, below Absolute Zero. Enter new temperature in Fahrenheiht greater than -460: ")))
+            degrees_fah = (int(input('Invalid temperature, below Absolute Zero. Enter new temperature in Fahrenheit that is greater than -460: ')))
         print (FtoC (degrees_fah))
 
     else:
         main()
 
-    restart = (input("Would you like to do another conversion?"))
+
+    restart = (input('Would you like to do another conversion?'))
     restart = restart.upper()
     if restart == "YES":
         main()
     else:
-        quit()
+        print('')
 
 print (main())
+
+###Citations
+#for making input uppercase:
+#https://stackoverflow.com/questions/8772142/converting-to-upper-case-which-way-is-more-pythonic
+#for making entire conversion input in a function 'main'
+#https://stackoverflow.com/questions/21372180/how-to-make-a-loop-repeat-itself-n-number-of-times-python-3
 
